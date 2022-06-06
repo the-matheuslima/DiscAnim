05fa1ad1cb953d524377fad3bc48609217705e7a
Matheus Lima <matheuslima.thedev@gmail.com>
2022-06-06 15:41:55 -0300

    add loading

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './style.scss'
+
+function index() {
+    return (
+        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+    );
+}
+
+export default index;
