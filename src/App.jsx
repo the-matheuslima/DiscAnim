4ffb804185ead2c30cddddf3254f1f117c7f2807
Matheus Lima <matheuslima.thedev@gmail.com>
2022-06-06 15:38:21 -0300

    add new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from './components/Pages/Home'
-import Detalins from './components/Pages/Detalins'
-import Navbar from "./components/Layout/Navbar/Navbar";
-import Search from "./components/Search/Search";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from './Pages/Home/index';
+import Detalins from './Pages/Detalins/index';
+import DetalinsAnin from './Pages/DetalinsAni/index';
+import Characters from './Pages/Characters/index';
+
+import Navbar from "./Layout/Navbar/index";
+import Search from "./components/Search/index";
+import AppProvider from './AppContext/Providers';
+import Footer from "./Layout/Footer/index";
 
 function App() {
-
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Search />} />
-        <Route path="/detalins/:id" element={<Detalins />} />
-      </Routes>
+      <AppProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Search />} />
+          <Route path="/detalins/" element={<Detalins />} />
+          <Route path="/detalins/:id/" element={<DetalinsAnin />} />
+          <Route path='/detalins/:id/characters' element={<Characters />} />
+        </Routes>
+      </AppProvider>
+      <Footer />
     </Router>
   )
 }
